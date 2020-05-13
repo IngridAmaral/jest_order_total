ebe50e074900f9f327c157e04f0f4319b0fa443d
Ingrid <guid_95@hotmail.com>
2020-05-13 17:06:03 +0200

    add tests

--- order-total.test.js
+++ order-total.test.js
@@ -1,13 +1,37 @@
-const orderTotal = require('./order-total');
+const orderTotal = require("./order-total");
 
-test('orderTotal function exists', () => {
-    expect(orderTotal).toBeDefined();
-});
+test("orderTotal function exists", () => expect(orderTotal).toBeDefined());
 
-test('the quantity is bigger than 1(default)', () => {
-    expect(orderTotal({
-        items: [
-          { 'name': 'Dragon candy', price: 2, quantity: 3 }
-        ]
-      })).toBe(6);
-});
+test("quantity specif", () =>
+  expect(
+    orderTotal({
+      items: [{ name: "Dragon candy", price: 2, quantity: 3 }],
+    })
+  ).toBe(6));
+
+test("quantity NOT specified", () =>
+  expect(
+    orderTotal({
+      items: [{ name: "Dragon candy", price: 2 }],
+    })
+  ).toBe(2));
+
+test("Adds 2 + 800 to equal 802", () =>
+  expect(
+    orderTotal({
+      items: [
+        { name: "Dragon candy", price: 2, quantity: 1 },
+        { name: "Dragon cage", price: 800, quantity: 1 },
+      ],
+    })
+  ).toBe(802));
+
+test("Adds 20 + 40 to equal 60", () =>
+  expect(
+    orderTotal({
+      items: [
+        { name: "Dragon candy", price: 20, quantity: 1 },
+        { name: "Dragon cage", price: 40, quantity: 1 },
+      ],
+    })
+  ).toBe(60));
